adca7c03e6493630d4da0760ad1e618f4db86023
appearing <appearing@3176436425.com>
2022-11-01 14:46:35 +0800

    test one

--- Much_Eat_Snack_Game/src/main/java/Hello.java
+++ Much_Eat_Snack_Game/src/main/java/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello world");
+        System.out.println("这个是test的分支改变代码");;
     }
 }
