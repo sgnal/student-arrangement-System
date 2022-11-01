e1c45c5758ddeea6f374e7f9c411f5972d3c1962
appearing <appearing@3176436425.com>
2022-11-01 14:34:21 +0800

    master 1

---
+++ Much_Eat_Snack_Game/src/main/java/Hello.java
@@ -0,0 +1,9 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
